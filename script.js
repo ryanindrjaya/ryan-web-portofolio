ee209b6155c08efd13f3a1f5221f3ca10588208a
Ryan Indrajaya <sryan062@gmail.com>
2022-06-28 03:11:12 +0700

    menambahkan project section

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 var typed = new Typed(".auto-type", {
-  strings: ["Developer.", "Techgeek.", "Musician."],
+  strings: ["developer.", "front-end developer.", "techgeek.", "design enthusiast."],
   typeSpeed: 130,
   backDelay: 2000,
   backSpeed: 100,
   loop: true
+});
+
+VanillaTilt.init(document.querySelectorAll(".card"), {
+  max: 4,
+  speed: 100,
+  glare: true,
+  'max-glare': 0.1,
 });
 
 function splitScroll() {
